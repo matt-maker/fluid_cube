9dc9cae85f59a0d821c14b99838f4a032c12ee49
matt-maker <68969720+matt-maker@users.noreply.github.com>
2024-02-10 18:34:51 +0000

    inserting bevy_prototype_lyon

--- src/grid.rs
+++ src/grid.rs
@@ -1,14 +1,10 @@
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
 
 pub const GRID_CELL_SIZE: Vec2 = Vec2::new(8.0, 8.0);
 pub const GRID_WIDTH: i32 = 150;
 pub const GRID_HEIGHT: i32 = 100;
 
 pub struct GridPlugin;
-
-#[derive(Component, Debug)]
-pub struct SimCell;
 
 #[derive(Component, Debug)]
 pub struct GridSmoke {
@@ -28,34 +24,7 @@ pub struct GridBundle {
 
 impl Plugin for GridPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, (spawn_cells, spawn_grid));
-    }
-}
-
-fn spawn_cells(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    for x in 0..GRID_WIDTH {
-        for y in 0..GRID_HEIGHT {
-            commands.spawn((
-                MaterialMesh2dBundle {
-                    mesh: meshes
-                        .add(Mesh::from(shape::Quad::new(GRID_CELL_SIZE)))
-                        .into(),
-                    transform: Transform::from_xyz(
-                        (x - GRID_WIDTH / 2) as f32 * GRID_CELL_SIZE[0],
-                        (y - GRID_HEIGHT / 2) as f32 * GRID_CELL_SIZE[1],
-                        0.0,
-                    ),
-                    material: materials
-                        .add(ColorMaterial::from(Color::rgba(255.0, 255.0, 255.0, 1.0))),
-                    ..default()
-                },
-                SimCell,
-            ));
-        }
+        app.add_systems(Startup, spawn_grid);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_prototype_lyon::prelude::*;
 
 mod camera_2d;
 mod grid;
@@ -9,8 +10,33 @@ use grid::GridPlugin;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .insert_resource(Msaa::Sample4)
         .add_plugins(DefaultPlugins)
+        .add_plugins(ShapePlugin)
         .add_plugins(CameraPlugin)
+        .add_systems(Startup, setup_system)
         .add_plugins(GridPlugin)
         .run();
+}
+
+fn setup_system(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let shape = shapes::RegularPolygon {
+        sides: 4,
+        feature: shapes::RegularPolygonFeature::Radius(4.0),
+        ..shapes::RegularPolygon::default()
+    };
+
+    commands.spawn((ShapeBundle {
+        path: GeometryBuilder::build_as(&shape),
+        material: materials.add(ColorMaterial::from(Color::rgb(100.0, 0.0, 0.0))),
+        spatial: SpatialBundle::from_transform(Transform::from_xyz(100.0, 0.0, 0.0)),
+        ..default()
+    },));
+
+    commands.spawn((ShapeBundle {
+        path: GeometryBuilder::build_as(&shape),
+        material: materials.add(ColorMaterial::from(Color::rgb(100.0, 0.0, 0.0))),
+        spatial: SpatialBundle::from_transform(Transform::from_xyz(150.0, 0.0, 0.0)),
+        ..default()
+    },));
 }
