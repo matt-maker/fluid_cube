17d4a1cc80592c48b30993853cd337c54ca5e11b
matt-maker <68969720+matt-maker@users.noreply.github.com>
2024-02-14 21:29:54 +0000

    added in cube data structure

--- src/grid.rs
+++ src/grid.rs
@@ -26,6 +26,138 @@ impl GridDensity {
 }
 
 #[derive(Component, Debug)]
+pub struct Size {
+    pub size: i32,
+}
+
+impl Size {
+    pub fn new(size: i32) -> Self {
+        Self { size }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Dt {
+    pub dt: f32,
+}
+
+impl Dt {
+    pub fn new(dt: f32) -> Self {
+        Self { dt }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Diff {
+    pub diff: f32,
+}
+
+impl Diff {
+    pub fn new(diff: f32) -> Self {
+        Self { diff }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Visc {
+    pub visc: f32,
+}
+
+impl Visc {
+    pub fn new(visc: f32) -> Self {
+        Self { visc }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct S {
+    pub s: Vec<f32>,
+}
+
+impl S {
+    pub fn new(s: Vec<f32>) -> Self {
+        Self { s }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Density {
+    pub density: Vec<f32>,
+}
+
+impl Density {
+    pub fn new(density: Vec<f32>) -> Self {
+        Self { density }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vx {
+    pub vx: Vec<f32>,
+}
+
+impl Vx {
+    pub fn new(vx: Vec<f32>) -> Self {
+        Self { vx }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vy {
+    pub vy: Vec<f32>,
+}
+
+impl Vy {
+    pub fn new(vy: Vec<f32>) -> Self {
+        Self { vy }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vz {
+    pub vz: Vec<f32>,
+}
+
+impl Vz {
+    pub fn new(vz: Vec<f32>) -> Self {
+        Self { vz }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vx0 {
+    pub vx0: Vec<f32>,
+}
+
+impl Vx0 {
+    pub fn new(vx0: Vec<f32>) -> Self {
+        Self { vx0 }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vy0 {
+    pub vy0: Vec<f32>,
+}
+
+impl Vy0 {
+    pub fn new(vy0: Vec<f32>) -> Self {
+        Self { vy0 }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Vz0 {
+    pub vz0: Vec<f32>,
+}
+
+impl Vz0 {
+    pub fn new(vz0: Vec<f32>) -> Self {
+        Self { vz0 }
+    }
+}
+
+#[derive(Component, Debug)]
 pub struct SimCells;
 
 #[derive(Component, Debug)]
@@ -33,6 +165,22 @@ pub struct Grid;
 
 #[derive(Bundle)]
 pub struct GridBundle {
+    pub size: Size,
+    pub dt: Dt,
+    pub diff: Diff,
+    pub visc: Visc,
+
+    pub s: S,
+    pub density: Density,
+
+    pub vx: Vx,
+    pub vy: Vy,
+    pub vz: Vz,
+
+    pub vx0: Vx0,
+    pub vy0: Vy0,
+    pub vz0: Vz0,
+
     pub grid_density: GridDensity,
 }
 
@@ -78,6 +226,22 @@ fn spawn_cells(mut commands: Commands) {
 fn spawn_grid(mut commands: Commands) {
     commands.spawn((
         GridBundle {
+            size: Size::new(0),
+            dt: Dt::new(0.0),
+            diff: Diff::new(0.0),
+            visc: Visc::new(0.0),
+
+            s: S::new(Vec::new()),
+            density: Density::new(Vec::new()),
+
+            vx: Vx::new(Vec::new()),
+            vy: Vy::new(Vec::new()),
+            vz: Vz::new(Vec::new()),
+
+            vx0: Vx0::new(Vec::new()),
+            vy0: Vy0::new(Vec::new()),
+            vz0: Vz0::new(Vec::new()),
+
             grid_density: GridDensity::new(Vec::new()),
         },
         Grid,
